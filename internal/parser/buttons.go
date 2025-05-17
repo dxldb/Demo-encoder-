fb4a8b0df7de76355d146181ea6f45ff795fe4d2
dxldb <114335970+dxldb@users.noreply.github.com>
2025-05-17 19:43:03 +0800

    Update buttons.go

--- internal/parser/buttons.go
+++ internal/parser/buttons.go
@@ -51,10 +51,10 @@ func ButtonConvert(player *common.Player, addonButton int32) int32 {
 		if zoomLevelProp != nil {
 			var zoomLevel = zoomLevelProp.Value().IntVal
 
-			if len(encoder.PlayerFramesMap[player.Name]) == 0 {
-				bufZoomLevelMap[player.Name] = 0
-			} else if bufZoomLevelMap[player.Name] != zoomLevel {
-				bufZoomLevelMap[player.Name] = zoomLevel
+			if len(encoder.PlayerFramesMap[player.SteamID64]) == 0 {
+				bufZoomLevelMap[player.SteamID64] = 0
+			} else if bufZoomLevelMap[player.SteamID64] != zoomLevel {
+				bufZoomLevelMap[player.SteamID64] = zoomLevel
 				button |= IN_ATTACK2
 			}
 		}
